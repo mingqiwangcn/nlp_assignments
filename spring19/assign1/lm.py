f3c612974d213c5d936a43bb78e5aef9ad0482be
mingqiwangcn <mingqiwangcn@gmail.com>
2019-04-03 23:06:38 -0500

    assign1

--- spring19/assign1/lm.py
+++ spring19/assign1/lm.py
@@ -1,7 +1,6 @@
 import random
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 
 EMBEDDING_DIM = 64
@@ -16,10 +15,9 @@ best_eval_accu = 0
 best_test_accu = 0
 
 
-class LSTMPredictor(nn.Module):
-    
+class LSTMLogLoss(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, corpus_size, labels_size):
-        super(LSTMPredictor, self).__init__()
+        super(LSTMLogLoss, self).__init__()
         self.hidden_dim = hidden_dim
         self.word_embeddings = nn.Embedding(corpus_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
@@ -29,10 +27,35 @@ class LSTMPredictor(nn.Module):
         N = len(word_idxs)
         embeds = self.word_embeddings(word_idxs)
         lstm_out, _ = self.lstm(embeds.view(N, 1, -1))
-        label_space = self.hidden_to_label(lstm_out.view(N, -1))
-        label_scores = F.log_softmax(label_space, dim=1)
-        return label_scores
-    
+        scores = self.hidden_to_label(lstm_out.view(N, -1))
+        return scores
+
+class LSTMBinaryLogLoss(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, corpus_size):
+        super(LSTMBinaryLogLoss, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.in_word_embeddings = nn.Embedding(corpus_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        
+    def forward(self, word_idxs, ):
+        N = len(word_idxs)
+        in_embeds = self.word_embeddings(word_idxs)
+        lstm_out, _ = self.lstm(in_embeds.view(N, 1, -1))
+        hidden_state = lstm_out.view(N, -1)
+        
+        return hidden_state
+
+class BinaryLogLoss(nn.Module):
+    def __init__(self, corpus_size, hidden_dim):
+        super(BinaryLogLoss, self).__init__()
+        self.out_word_embeddings = nn.Embedding(corpus_size, hidden_dim)
+        self.log_sig = nn.LogSigmoid()
+    def forward(self, hidden_state, out_embeds):
+        scores = (hidden_state * out_embeds).sum(dim=1)
+        sig_scores = -self.log_sig(scores)
+        
+        return
+
 def load_corpus():
     path = "./31210-s19-hw1/bobsue.voc.txt"
     file = open(path, "r")
@@ -84,7 +107,7 @@ def train():
     corpus_size = len(word_to_idx)
     labels_size = corpus_size
     model = LSTMPredictor(EMBEDDING_DIM, HIDDEN_DIM, corpus_size, labels_size)
-    loss_fn = nn.NLLLoss()
+    loss_fn = nn.CrossEntropyLoss()
     learing_rate = 1e-3
     optimizer = optim.Adam(model.parameters(), lr = learing_rate)
     N = len(training_data)
@@ -92,7 +115,7 @@ def train():
     for epoc in range(EPOCS):
         if epoc > 0:
             random.shuffle(training_data)
-        iter = 0
+        itr = 0
         for X, Y in training_data:
             model.zero_grad()
             ts_X = torch.tensor(X, dtype = torch.long)
@@ -101,8 +124,8 @@ def train():
             loss = loss_fn(label_scores, ts_Y)
             loss.backward()
             optimizer.step()
-            iter += 1
-            if (iter % M == 0 or iter == N):
+            itr += 1
+            if (itr % M == 0 or itr == N):
                 print(epoc, loss.item())
                 evaluate(epoc, model)
     
