bf555494ab67db2c2d95359e308863f11534420e
mingqiwangcn <mingqiwangcn@gmail.com>
2019-06-14 18:55:39 -0500

    assign5

--- spring19/assign5/q1.py
+++ spring19/assign5/q1.py
@@ -283,7 +283,7 @@ def main():
     
     str_dist_type = (str(char_dist_type).split('.'))[1]
     str_dist_type = str_dist_type.lower()
-    print("num_itr=%d beta=%.1f gamma=%.1f s=%.1f char_dist_type=%s start_prob_tao=%.1f" \
+    print("num_itr=%d beta=%.1f gamma=%.1f s=%.1f char_dist_type=%s start_prob_tao=%.2f" \
           %(num_itr, beta, gamma, s, str_dist_type, start_prob_tao ))
         
     np.random.seed(1)
